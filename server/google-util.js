e584f7857d4e8a68046e04c966bf2fcb9bc63477
pauline <pauline.feytel@gmail.com>
2019-11-01 20:48:43 +0100

    Add login component and googleapis for redirecting to google login

---
+++ server/google-util.js
@@ -0,0 +1,55 @@
+const { google } = require('googleapis');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_REDIRECT = process.env.GOOGLE_REDIRECT;
+
+const googleConfig = {
+  clientId: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  redirect: GOOGLE_REDIRECT
+};
+
+/**
+ * Create the google auth object which gives us access to talk to google's apis.
+ */
+function createConnection() {
+  return new google.auth.OAuth2(
+    googleConfig.clientId,
+    googleConfig.clientSecret,
+    googleConfig.redirect
+  );
+}
+
+/**
+ * This scope tells google what information we want to request.
+ */
+const defaultScope = [
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/userinfo.email',
+];
+
+/**
+ * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
+ */
+function getConnectionUrl(auth) {
+  return auth.generateAuthUrl({
+    access_type: 'offline',
+    prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+    scope: defaultScope
+  });
+}
+
+/**
+ * Create the google url to be sent to the client.
+ */
+function urlGoogle() {
+  const auth = createConnection();
+  const url = getConnectionUrl(auth);
+  return url;
+}
+
+module.exports = urlGoogle;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require('path');
+const googleUtil = require('./google-util');
 
-app.get('/api/product', (req,res) => {
+app.get('/api/product', (req, res) => {
     const list = ['tomato', 'banana', 'chocolate'];
     res.json(list);
+});
+
+app.get('/api/login', (req, res) => {
+  res.send(googleUtil());
 });
 
 app.listen(PORT, () => {
